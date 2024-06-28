a82989d42df098f224d977fc631e8523b9af170d
johnmayor723 <36506734+johnmayor723@users.noreply.github.com>
2024-06-28 12:54:21 -0700

    mooo

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const ejs = require('ejs')
 
 const port = process.env.port || 3000
 
+//app configurations
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-
+//routes
 app.get('/', (req, res)=>{
    res.render('index') 
 })
+
+app.get('/about', (req, res)=>{
+    res.render('about') 
+ })
+
+ app.get('/contact', (req, res)=>{
+    res.render('contact') 
+ })
+ 
+ app.get('/courses', (req, res)=>{
+    res.render('course') 
+ })
+
+
 app.listen(port, ()=>{
-    console.log(`server started listeming on ${port}`)
+    console.log(`server started listening on ${port}`)
 })
